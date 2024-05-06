59995d01ab997a41950929a09b0dd9783e6e1c56
gabrielrmello515 <gabrielrmello515@gmail.com>
2024-05-06 19:32:34 +0000

    change to top_bar_widget

---
+++ lib/home/components/top_bar_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:geekcontrol/home/atoms/search_page.dart';
+
+class TopBarWidget extends StatelessWidget {
+  const TopBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: const Text("GeekControl"),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.search),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SearchPage()),
+            );
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.notifications),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: const Icon(Icons.settings),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
