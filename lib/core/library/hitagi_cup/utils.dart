ed112cfdfc4dcf568dc763023a2c56fa252b3d2d
gabrielrmello515 <gabrielrmello515@gmail.com>
2024-05-22 22:34:40 +0000

    add maxCharacters method

--- lib/core/library/hitagi_cup/utils.dart
+++ lib/core/library/hitagi_cup/utils.dart
@@ -2,6 +2,13 @@ import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 class Utils {
+  static String maxCharacters(int maxCharacters, {required String text}) {
+    if (text.length > maxCharacters) {
+      return '${text.substring(0, maxCharacters)}...';
+    }
+    return text;
+  }
+
   static String timeFromMSeconds(int dateTimestamp) {
     initializeDateFormatting('pt_BR', null);
     DateTime dateTime =
