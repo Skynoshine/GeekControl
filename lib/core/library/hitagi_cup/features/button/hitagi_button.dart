a3138ec7b2ed059fe8dc81947a4b447e773e70b5
gabrielrmello515 <gabrielrmello515@gmail.com>
2024-05-22 22:34:21 +0000

    fix button component

--- lib/core/library/hitagi_cup/features/button/hitagi_button.dart
+++ lib/core/library/hitagi_cup/features/button/hitagi_button.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import '../text/hitagi_text.dart';
 
+enum IconPosition {
+  left,
+  right,
+}
+
 class HitagiButton extends TextButton {
   final String label;
   final Color? corButton;
@@ -8,6 +13,7 @@ class HitagiButton extends TextButton {
   final EdgeInsetsGeometry padding;
   final double? elevation;
   final HitagiTypography typography;
+  final IconPosition iconPosition;
 
   HitagiButton({
     super.key,
@@ -18,6 +24,7 @@ class HitagiButton extends TextButton {
     this.padding = const EdgeInsets.symmetric(horizontal: 32),
     this.elevation,
     this.typography = HitagiTypography.button,
+    this.iconPosition = IconPosition.left,
   }) : super(
           child: HitagiText(
             text: label,
@@ -34,6 +41,7 @@ class HitagiButton extends TextButton {
             padding: padding,
           ),
         );
+
   HitagiButton.icon({
     super.key,
     required this.label,
@@ -44,21 +52,32 @@ class HitagiButton extends TextButton {
     this.elevation,
     this.typography = HitagiTypography.button,
     required IconData icon,
+    this.iconPosition = IconPosition.left,
   }) : super(
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
+              if (iconPosition == IconPosition.left) ...[
+                Icon(
+                  icon,
+                  size: 18,
+                  color: Colors.black,
+                ),
+                const SizedBox(width: 6),
+              ],
               HitagiText(
                 text: label,
                 typography: typography,
-                color: corTexto ?? Colors.white,
+                color: corTexto ?? const Color.fromARGB(255, 0, 0, 0),
               ),
-              const SizedBox(width: 6),
-              Icon(
-                icon,
-                size: 18,
-                color: Colors.white,
-              ),
+              if (iconPosition == IconPosition.right) ...[
+                const SizedBox(width: 6),
+                Icon(
+                  icon,
+                  size: 18,
+                  color: Colors.black,
+                ),
+              ],
             ],
           ),
           style: TextButton.styleFrom(
@@ -79,6 +98,7 @@ class HitagiButton extends TextButton {
     this.corTexto,
     this.padding = const EdgeInsets.symmetric(horizontal: 32),
     this.elevation,
+    this.iconPosition = IconPosition.left,
     this.typography = HitagiTypography.button,
   }) : super(
           child: HitagiText(
